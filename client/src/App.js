59e10b7d428865a17489e7d75c0baacb7853f3f0
David Mondragon <dragon18saiz@gmail.com>
2023-08-08 19:28:22 -0600

    added react-router-dom, added proper routes to App.js, modified connection.js (db name), modified server (previous to queries and mutation built)

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
+// import React from 'react';
+// import Head from './components/Head';
+// import '../src/styles/globals.css';
+
+// function App() {
+//   return <Head />;
+// }
+
+// export default App;
+
 import React from 'react';
-import Head from './components/Head';
-import '../src/styles/globals.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Uncomment import statement below after building queries and mutations
+// import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+import Head from './pages/Head';
 
 function App() {
-  return <Head />;
+  return (
+    <Router>
+      <div className="flex-column justify-center align-center min-100-vh bg-primary">
+        <Routes>
+          <Route path="/" element={<Head />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;

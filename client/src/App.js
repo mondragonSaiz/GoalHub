b2385848c9ee6c438d13667e92029ffd567e418d
EduardoPinedaH <eduardopinedahu@gmail.com>
2023-08-08 20:41:35 -0600

    tailwindcss fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 // import React from 'react';
 // import Head from './components/Head';
-// import '../src/styles/globals.css';
+// 
 
 // function App() {
 //   return <Head />;
@@ -9,12 +9,12 @@
 // export default App;
 
 import React from 'react';
+import Head from './pages/Head';
+import '../src/styles/globals.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Uncomment import statement below after building queries and mutations
 // import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-
-import Head from './pages/Head';
 
 function App() {
   return (

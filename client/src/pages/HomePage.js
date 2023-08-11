f892e37a97d64d36230f32204666111ef3ce1548
EduardoPinedaH <eduardopinedahu@gmail.com>
2023-08-10 20:51:50 -0600

    HomePage fixed and signup changes

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -21,37 +21,37 @@ export default function HomePage() {
             </div>
             <div className='mt-10 lg:w-1/3'>
               <p className='text-slate-200 font-normal mt-10 w-auto lg:text-lg font-poppins'>
-                Write, plan, collaborate and organize your goals.
+                Write, plan, collaborate and organize your <strong>goals</strong>.
               </p>
               <p className='text-slate-200 font-normal mt-2 w-4/6 lg:text-lg lg:w-11/12 font-poppins'>
-                GoalHub is all you need - in one space.
+                <strong>GoalHub</strong> is all you need - in one space.
               </p>
               <div className='mt-10'>
                 <input 
                   placeholder='Enter your email.'
                   type="text"
-                  className=' focus:text-slate-200 text-slate-200 bg-neutral-950 border-2 rounded-lg border-gray-500 text-left pl-4 py-1 mr-4 lg:text-lg lg:w-3/5'
+                  className=' focus:text-slate-200 text-slate-200 bg-neutral-950 border-2 rounded-lg border-gray-500 text-left pl-4 py-1 mr-4 lg:text-lg lg:w-3/5 xl:w-1/2'
                 />
-                <a href="/sign-up" className=' bg-slate-200 text-neutral-950 py-2 px-6 font-mediumfont-poppins rounded-lg lg:text-lg cursor-pointer'>Get Started</a>
+                <a href="/sign-up" className=' bg-slate-200 text-neutral-950 py-2 px-6 font-bold font-poppins rounded-lg lg:text-lg cursor-pointer'>Get Started</a>
               </div>
             </div>
           </div>
           <div className='flex flex-row flex-wrap mt-10 justify-center gap-20 lg:flex-nowrap'>
               <div className='flex flex-col'>
                 <img src={avatar3} alt="avatar3" className='w-64 h-auto -mb-5 lg:w-72 lg:hover:w-80'/>
-                <p className=' text-slate-200 font-normal text-center lg:text-base'>Keep tasks organized</p>
+                <p className=' text-slate-200 font-normal text-center lg:text-base'>Keep tasks <strong>organized</strong>.</p>
               </div>
               <div>
                 <img src={avatar2} alt="avatar2" className='w-64 h-auto -mb-5 lg:w-72 lg:hover:w-80'/>
-                <p className='  text-slate-200 font-normal text-center lg:text-base'>Find out others goals</p>
+                <p className='  text-slate-200 font-normal text-center lg:text-base'>Find out others <strong>goals</strong>.</p>
               </div>
               <div>
                 <img src={avatar4} alt="avatar4" className='w-64 h-auto -mb-5 lg:w-72 lg:hover:w-80'/>
-                <p className='  text-slate-200 font-normal text-center lg:text-base'>Increase productivity</p> 
+                <p className='  text-slate-200 font-normal text-center lg:text-base'>Increase <strong>productivity</strong>.</p> 
               </div>
               <div>
                 <img src={avatar5} alt="avatar5" className='w-64 h-auto -mb-5 lg:w-72 lg:hover:w-80'/>
-                <p className='  text-slate-200 font-normal text-center lg:text-base'>Motivate others</p>
+                <p className='  text-slate-200 font-normal text-center lg:text-base'>Motivate <strong>others</strong>.</p>
               </div>
           </div>
         </section>
--- client/src/pages/signUp.js
+++ client/src/pages/signUp.js
@@ -10,9 +10,9 @@ export default function signUp() {
         <div className="font-poppins">
             <main className="flex justify-center bg-neutral-950 px-10 md:px-20 lg:px-40">
                 <section className="flex min-h-screen">
-                    <div className="flex flex-col justify-center items-center lg:-mt-20">
+                    <div className="flex flex-col justify-center items-center lg:-mt-20 mt-10 mb-10">
                         <div className="flex flex-col items-center w-auto border-2 rounded-2xl border-slate-200 px-14 py-14 gap-8">
-                            <h1 className="text-slate-200 font-bold text-4xl mb-5 text-center">Join as an employee or employer</h1>
+                            <h1 className="text-slate-200 text-2xl font-bold lg:text-4xl mb-5 text-center">Join as an employee or employer</h1>
                             <div className="flex flex-col lg:flex-row gap-16">
                                 <div className="w-80 h-auto border-2 rounded-2xl border-slate-200 hover:border-4 cursor-pointer lg:cursor-default">
                                     <button onClick={handleClick} className="visible btn-employee border-slate-200 border-2 rounded-full w-10 h-10 mt-4 ml-64 cursor-pointer mb-10"></button>

681a38ba24dddce6fd9010774b73c7589241140c
alexslzr <alejandroslzr17@gmail.com>
2023-08-12 23:22:51 -0600

    changed password for user model, commented tailwing-merge on area model, changed await for return on bcrypt comparemethod for user model, added typedef of existing model, added login and add user mutation, and query for user and users

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,30 +1,48 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Thought {
+  type User {
     _id: ID
-    thoughtText: String
-    thoughtAuthor: String
-    createdAt: String
-    comments: [Comment]!
+    firstName: String
+    lastName: String
+    email: String
+    password: String
+    isEmployee: Boolean
+    progress: Int
+    area: Area
   }
 
-  type Comment {
+  type Area {
     _id: ID
-    commentText: String
+    name: String
+    supervisor: String
+    progress: String
+    users: [User]
+  }
+
+  type Task {
+    _id: ID
+    name: String
+    taskDesc: String
     createdAt: String
+    isCompleted: Boolean
+    area: Area
+    user: User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 
   type Query {
-    thoughts: [Thought]!
-    thought(thoughtId: ID!): Thought
+    users: [User]
+    user(email: String!): User
   }
 
   type Mutation {
-    addThought(thoughtText: String!, thoughtAuthor: String!): Thought
-    addComment(thoughtId: ID!, commentText: String!): Thought
-    removeThought(thoughtId: ID!): Thought
-    removeComment(thoughtId: ID!, commentId: ID!): Thought
+    addUser(firstName: String!,lastName: String!,isEmployee: Boolean!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
   }
 `;
 

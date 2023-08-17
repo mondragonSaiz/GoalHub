6362661d6596bfc2e1449ee08673f57f8c3e1652
EduardoPinedaH <eduardopinedahu@gmail.com>
2023-08-17 14:09:28 -0600

    Sign up form style updates

--- client/src/pages/SignUpForm.js
+++ client/src/pages/SignUpForm.js
@@ -143,8 +143,8 @@ export default function SignUpForm() {
     <div className=" font-poppins">
       <main className="flex justify-center bg-neutral-950">
         <section className="flex min-h-screen">
-          <div className="flex flex-col justify-center items-center lg:-mt-20">
-            <div className="flex flex-col items-center w-auto border-2 rounded-2xl border-slate-200 px-14 py-14 gap-8">
+          <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col items-center w-auto border-2 rounded-2xl border-slate-200 px-14 py-2 gap-8">
               {data ? (
                 <p>
                   Directing to your Dashboard <Link to="/"> Moving</Link>
